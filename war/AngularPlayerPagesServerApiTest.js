a6cde58035fcea04f844eb630c39f353d204e466
Pinji Zhao <pz454@nyu.edu>
2014-05-12 18:00:01 -0400

    Final Assignment - Server Api Test

--- war/AngularPlayerPagesServerApiTest.js
+++ war/AngularPlayerPagesServerApiTest.js
@@ -10,6 +10,8 @@ var shortPwdPlayer = {
 		"password": "pwd"		
 };
 
+var playerId;
+var accessSignature;
 
 QUnit.config.reorder = false;
 
@@ -23,12 +25,12 @@ function ajaxInsertNewPlayer(expected)
     dataType: 'json',
     data: JSON.stringify(jsonObj),
     success: function(data, textStatus, jqXHR) {     
-      console.log(data);
+      //console.log(data);
       if(data["error"] != "EMAIL_EXISTS" && data["error"] != "PASSWORD_TOO_SHORT" &&
          expected == ""
          ) {
         accessSignature = data.accessSignature;
-        playerId = data.playerId
+        PlayerId = data.playerId
         ok(true);
       }
       else if(data["error"] == "EMAIL_EXISTS" && expected == "EMAIL_EXISTS")
@@ -43,13 +45,273 @@ function ajaxInsertNewPlayer(expected)
       {
     	ok(false);
       }
-
       start();
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.error("ERROR: " + textStatus + " " + errorThrown);
     }
     });
+};
+
+function ajaxGetPlayerInfo(player, target, sig, expected){
+	$.ajax({
+		url: sServer+'/playerInfo?playerId='+player+'&targetId='+target+'&accessSignature='+sig,
+		type: 'GET', 
+		dataType: 'json', 
+		success: function(data, textStatus, jqXHR){
+			//console.log(data);
+			if(data["error"]=="WRONG_PLAYER_ID" && expected == "WRONG_PLAYER_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_ACCESS_SIGNATURE"
+				&& expected == "WRONG_ACCESS_SIGNATURE"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_TARGET_ID" && expected == "WRONG_TARGET_ID"){
+				ok(true);
+			}
+			else if(data != null && expected == ""){
+				ok(true);
+			}
+			else{
+				ok(false);
+			}
+			start();	
+		},
+	error: function(jqXHR, textStatus, errorThrown){
+		console.error("ERROR: "+textStatus +" "+errorThrown);
+	}
+	});
+};
+
+function ajaxGetAllGameList(expected)
+{
+	$.ajax({
+		url: sServer + "/gameinfo/all", 
+		type: 'GET',
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR) {     
+			//console.log(data);
+			totalNum = data.length;
+			game = data[0];
+			console.log(game);
+			if (expected == "") {
+				ok(true);
+			} else {
+				ok(false);
+			}
+			start();
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.error("ERROR: " + textStatus + " " + errorThrown);
+		}
+	});
+}
+
+function ajaxGetHistoryDetail(player, target, game, sig, expected){
+	$.ajax({
+		url: sServer+'/history?playerId='+player+'&targetId='+target+'&gameId='+game+'&accessSignature='+sig,
+		type: 'GET', 
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR){
+			console.log(data);
+			if(data["error"]=="WRONG_PLAYER_ID" && expected == "WRONG_PLAYER_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_ACCESS_SIGNATURE" && expected == "WRONG_ACCESS_SIGNATURE"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_TARGET_ID" && expected == "WRONG_TARGET_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_GAME_ID" && expected == "WRONG_GAME_ID"){
+				ok(true);
+			}
+			else if(data != null && expected == ""){
+				ok(true);
+			}
+			else{
+				ok(false);
+			}
+			start();	
+		},
+	error: function(jqXHR, textStatus, errorThrown){
+		console.error("ERROR: "+textStatus +" "+errorThrown);
+	}
+	});
+};
+
+function ajaxGetHistoryList(player, target, sig, expected){
+	$.ajax({
+		url: sServer+'/playerAllGame?playerId='+player+'&targetId='+target+'&accessSignature='+sig,
+		type: 'GET', 
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR){
+			console.log(data);
+			if(data["error"]=="WRONG_PLAYER_ID" && expected == "WRONG_PLAYER_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_ACCESS_SIGNATURE" && expected == "WRONG_ACCESS_SIGNATURE"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_TARGET_ID" && expected == "WRONG_TARGET_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_GAME_ID" && expected == "WRONG_GAME_ID"){
+				ok(true);
+			}
+			else if(data != null && expected == ""){
+				ok(true);
+			}
+			else{
+				ok(false);
+			}
+			start();	
+		},
+	error: function(jqXHR, textStatus, errorThrown){
+		console.error("ERROR: "+textStatus +" "+errorThrown);
+	}		
+	});
+};
+
+function ajaxGetGameInfo(player, target, game, sig, expected){
+	$.ajax({
+		url: sServer+'/history?playerId='+player+'&targetId='+target+'&gameId='+game+'&accessSignature='+sig,
+		type: 'GET', 
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR){
+			console.log(data);
+			if(data["error"]=="WRONG_PLAYER_ID" && expected == "WRONG_PLAYER_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_ACCESS_SIGNATURE" && expected == "WRONG_ACCESS_SIGNATURE"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_TARGET_ID" && expected == "WRONG_TARGET_ID"){
+				ok(true);
+			}
+			else if(data["error"]=="WRONG_GAME_ID" && expected == "WRONG_GAME_ID"){
+				ok(true);
+			}
+			else if(data != null && expected == ""){
+				ok(true);
+			}
+			else{
+				ok(false);
+			}
+			start();	
+		},
+	error: function(jqXHR, textStatus, errorThrown){
+		console.error("ERROR: "+textStatus +" "+errorThrown);
+	}
+	});
+};
+
+function ajaxPostRate(gameId, playerId, accessSignature, rating, expected)
+{
+	var jsonObj = {
+		"gameId": gameId,
+		"playerId": playerId,
+		"accessSignature": accessSignature,
+		"rating": rating
+	}
+	$.ajax({
+		url: sServer + "/gameinfo/rating", 
+		type: 'POST',
+		dataType: 'json',
+		data: JSON.stringify(jsonObj),
+		success: function(data, textStatus, jqXHR) {     
+		console.log(data);
+		if ( data["error"] != "WRONG_RATING" &&
+			data["error"] != "WRONG_GAME_ID" &&
+			data["error"] != "WRONG_ACCESS_SIGNATURE" &&
+			expected == ""
+		) {
+			ok(true);
+		}
+		else if(data["error"] == "WRONG_RATING" &&
+			expected == "WRONG_RATING")
+		{
+			ok(true);
+		}
+		else if(data["error"] == "WRONG_GAME_ID" &&
+			expected == "WRONG_GAME_ID")
+		{
+			ok(true);
+		}
+		else if(data["error"] == "WRONG_ACCESS_SIGNATURE" &&
+			expected == "WRONG_ACCESS_SIGNATURE")
+		{
+			ok(true);
+		}
+		else
+		{
+			ok(false);
+		}
+		start();
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.error("ERROR: " + textStatus + " " + errorThrown);
+    }
+    });
+};
+
+function ajaxGetGameStat(id, expected)
+{
+	$.ajax({
+		url: sServer + "/gameinfo/stats?gameId=" + id, 
+		type: 'GET',
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR) {     
+			console.log(data);
+			if (data["error"] == "NO_MATCH_RECORDS" &&
+				expected == "NO_MATCH_RECORDS")
+			{
+				ok(true);
+			} else if (data["error"] == "WRONG_GAME_ID" &&
+				expected == "WRONG_GAME_ID")
+			{
+				ok(true);
+			} else if (expected == "") {
+				ok(true);
+			} else {
+				ok(false);
+			}
+		    start();
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.error("ERROR: " + textStatus + " " + errorThrown);
+		}
+	});
+};
+
+function ajaxDeletePlayer(id, sig, expected)
+{
+	$.ajax({
+		url: sServer + "/players/" + id + "?accessSignature=" + sig, 
+		type: 'DELETE',
+		dataType: 'json',
+		success: function(data, textStatus, jqXHR) {     
+			console.log(data);
+			if (data["error"] == "WRONG_PLAYER_ID" &&
+				expected == "WRONG_PLAYER_ID")
+			{
+				ok(true);
+			} else if (data["error"] == "WRONG_ACCESS_SIGNATURE" &&
+				expected == "WRONG_ACCESS_SIGNATURE")
+			{
+				ok(true);
+			} else if (data["success"] == "DELETED_PLAYER" && expected == "") {
+				ok(true);
+			} else {
+				ok(false);
+			}
+		    start();
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.error("ERROR: " + textStatus + " " + errorThrown);
+		}
+	});
 };
 
 asyncTest("Insert New User Success", function() {
@@ -67,4 +329,116 @@ asyncTest("Insert New User With Existed Email", function() {
 asyncTest("Insert New User With Short Password", function() {
 	newPlayer = shortPwdPlayer;
     ajaxInsertNewPlayer("PASSWORD_TOO_SHORT");
+});
+
+asyncTest( "Get Player's Info With WRONG_ACCESS_SIGNATURE", function() {
+	ajaxGetPlayerInfo(PlayerId, PlayerId, "", "WRONG_ACCESS_SIGNATURE");
+});
+
+asyncTest( "Get Player's Info With WRONG_PLAYER_ID", function() {
+	ajaxGetPlayerInfo("", PlayerId, accessSignature, "WRONG_PLAYER_ID");
+});
+
+asyncTest( "Get Player's Info With WRONG_TARGET_ID", function() {
+	ajaxGetPlayerInfo(PlayerId, "", accessSignature, "WRONG_TARGET_ID");
+});
+
+asyncTest( "Get Player's Info Correctly", function() {
+	ajaxGetPlayerInfo(PlayerId, PlayerId, accessSignature, "");
+});
+
+asyncTest( "Get All Game Correctly", function() {
+	ajaxGetAllGameList("");
+});
+
+asyncTest( "Get Player's History Detail With WRONG_TARGET_ID", function() {
+	ajaxGetHistoryDetail(PlayerId, "", game.gameId, accessSignature, "WRONG_TARGET_ID");
+});
+
+asyncTest("Get Player's History Detail With WRONG_PLAYER_ID", function() {
+	ajaxGetHistoryDetail("", PlayerId, game.gameId, accessSignature, "WRONG_PLAYER_ID");
+});
+
+asyncTest( "Get Player's History Detail With WRONG_ACCESS_SIGNATURE", function() {
+	ajaxGetHistoryDetail(PlayerId, PlayerId, game.gameId, "", "WRONG_ACCESS_SIGNATURE");
+});
+
+asyncTest( "Get Player's History Detail Correctly", function() {
+	ajaxGetHistoryDetail(PlayerId, PlayerId, game.gameId, accessSignature, "");
+});
+
+asyncTest( "Get History List With WRONG_TARGET_ID", function() {
+	ajaxGetHistoryList(PlayerId, "", accessSignature, "WRONG_TARGET_ID");
+});
+
+asyncTest( "Get History List With WRONG_PLAYER_ID", function() {
+	ajaxGetHistoryList("", PlayerId, accessSignature, "WRONG_PLAYER_ID");
+});
+
+asyncTest( "Get History List With WRONG_ACCESS_SIGNATURE", function() {
+	ajaxGetHistoryList(PlayerId, PlayerId, "", "WRONG_ACCESS_SIGNATURE");
+});
+
+asyncTest( "Get History List Correctly", function() {
+	ajaxGetHistoryList(PlayerId, PlayerId, accessSignature, "");
+});
+
+asyncTest( "Get Game Token With WRONG_TARGET_ID", function() {
+	ajaxGetGameInfo(PlayerId, "", game.gameId, accessSignature, "WRONG_TARGET_ID");
+});
+
+asyncTest( "Get Game Token With WRONG_PLAYER_ID", function() {
+	ajaxGetGameInfo("", PlayerId, game.gameId, accessSignature, "WRONG_PLAYER_ID");
+});
+
+asyncTest( "Get Game Token With WRONG_ACCESS_SIGNATURE", function() {
+	ajaxGetGameInfo(PlayerId, PlayerId, game.gameId, "", "WRONG_ACCESS_SIGNATURE");
+});
+
+asyncTest( "Get Game Token With WRONG_GAME_ID", function() {
+	ajaxGetGameInfo(PlayerId, PlayerId, "", accessSignature, "WRONG_GAME_ID");
+});
+
+asyncTest( "Get Game Token Correctly", function() {
+	ajaxGetGameInfo(PlayerId, PlayerId, game.gameId, accessSignature, "");
+});
+
+asyncTest("Rate A Game Correctly", function() {
+    var goodRate = "5.0";
+    ajaxPostRate(game.gameId, PlayerId, accessSignature, goodRate, "");
+});
+
+asyncTest("Rate A Game With WRONG_RATING", function() {
+    var badRate = "500";
+    ajaxPostRate(game.gameId, PlayerId, accessSignature, badRate, "WRONG_RATING");
+});
+
+asyncTest("Rate A Game With WRONG_GAME_ID", function() {
+    var goodRate = "5.0";
+    ajaxPostRate("1111111", PlayerId, accessSignature, goodRate, "WRONG_GAME_ID");
+});
+
+asyncTest("Rate A Game With WRONG_ACCESS_SIGNATURE", function() {
+    var goodRate = "5.0";
+    ajaxPostRate(game.gameId, PlayerId, "", goodRate, "WRONG_ACCESS_SIGNATURE");
+});
+
+asyncTest("Get Game Stat Correctly", function() {
+    ajaxGetGameStat(game.gameId, "");
+});
+
+asyncTest("Get Game Stat With WRONG_GAME_ID", function() {
+    ajaxGetGameStat("1234", "WRONG_GAME_ID");
+});
+
+asyncTest("Delete Player With WRONG_PLAYER_ID", function() {
+	ajaxDeletePlayer("", accessSignature, "WRONG_PLAYER_ID");
+});
+
+asyncTest("Delete Player With WRONG_ACCESS_SIGNATURE", function() {
+	ajaxDeletePlayer(PlayerId, "", "WRONG_ACCESS_SIGNATURE");
+});
+
+asyncTest("Delete Player Correctly", function() {
+	ajaxDeletePlayer(PlayerId, accessSignature, "");
 });
